d251dee4c950edd6ec314dd1083a01e960eaf0bc
tungleqb <42248230+tungleqb@users.noreply.github.com>
2023-09-29 11:29:57 +0700

    Create multitori.py

---
+++ multitori.py
@@ -0,0 +1,22 @@
+import subprocess
+import multiprocessing
+def command1():
+  # Lệnh hoặc công việc bạn muốn chạy ở lệnh đầu tiên
+  subprocess.run(['python', '/content/XENGPUMiner/miner.py', '--gpu=true'])
+
+def command2():
+  # Lệnh hoặc công việc bạn muốn chạy ở lệnh thứ hai
+  subprocess.run(['nohup','/content/XENGPUMiner/xengpuminer', '-b', '1024'])
+
+if __name__ == '__main__':
+  # Tạo hai tiến trình riêng biệt cho mỗi lệnh
+  process1 = multiprocessing.Process(target=command1)
+  process2 = multiprocessing.Process(target=command2)
+
+  # Bắt đầu chạy các tiến trình
+  process1.start()
+  process2.start()
+
+  # Chờ cho đến khi các tiến trình hoàn thành
+  process1.join()
+  process2.join()
