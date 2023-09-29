8c25d98338e3301154a5af9afa1963110aec0751
tungleqb <42248230+tungleqb@users.noreply.github.com>
2023-09-29 11:38:36 +0700

    Update multitori.py

--- multitori.py
+++ multitori.py
@@ -2,7 +2,7 @@ import subprocess
 import multiprocessing
 def command1():
   # Lệnh hoặc công việc bạn muốn chạy ở lệnh đầu tiên
-  subprocess.run(['python', '/content/XENGPUMiner/miner.py', '--gpu=true'])
+  subprocess.run(['python', '/content/ToriXEMminer/miner.py', '--gpu=true'])
 
 def command2():
   # Lệnh hoặc công việc bạn muốn chạy ở lệnh thứ hai
